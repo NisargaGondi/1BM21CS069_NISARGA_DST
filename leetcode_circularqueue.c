24176300165830473555c64e231907c66ebbebe3
NisargaGondi <118426071+NisargaGondi@users.noreply.github.com>
2023-01-13 15:30:26 +0530

    Create leetcode_circularqueue.c

---
+++ leetcode_circularqueue.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool hasCycle(struct ListNode *head) {
+
+    if(head==NULL)
+    return false;
+struct ListNode *slow = head;
+struct ListNode *fast = slow->next;
+while(slow!=fast)
+{
+    if(fast==NULL)
+        return false;
+    fast = fast->next;
+    if(fast==NULL)
+        return false;
+    fast = fast->next;
+    slow = slow->next;
+   
+}
+    if(fast==NULL)
+    return false;
+ return true;  
+}
